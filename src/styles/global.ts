62fbdd6b5180de7dcbe74d5171f5f2d6be6dbe25
Matheus Lino <matheuslino86@gmail.com.br>
2023-12-12 08:11:05 -0400

    feat: :sparkles: Global improvments

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,16 @@
-import { createGlobalStyle, css } from 'styled-components'
+import {
+  DefaultTheme,
+  GlobalStyleComponent,
+  createGlobalStyle,
+  css
+} from 'styled-components'
 
+type GlobalStylesProps = {
+  removeBg?: boolean
+}
 
-const GlobalStyles = createGlobalStyle`
+const GlobalStyles: GlobalStyleComponent<GlobalStylesProps, DefaultTheme> =
+  createGlobalStyle`
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
@@ -35,9 +44,14 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
   }
 
-  ${({ theme }) => css`
+  ${({ theme, removeBg }) => css`
     html {
       font-size: 62.5%;
     }
@@ -45,6 +59,11 @@ const GlobalStyles = createGlobalStyle`
     body {
       font-family: ${theme.font.family};
       font-size: ${theme.font.sizes.medium};
+
+      ${!removeBg &&
+      css`
+        background-color: ${theme.colors.mainBg};
+      `}
     }
   `}
 `
