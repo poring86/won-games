c6514348656dda2ae33f07e403e8449d57591792
Matheus Lino <matheuslino86@gmail.com.br>
2023-12-14 19:02:30 -0400

    feat: :sparkles: Radio

---
+++ src/components/Radio/index.tsx
@@ -0,0 +1,45 @@
+import { InputHTMLAttributes } from 'react'
+import * as S from './styles'
+
+type RadioValue = string | ReadonlyArray<string> | number
+
+export type RadioProps = {
+  onCheck?: (value?: RadioValue) => void
+  isChecked?: boolean
+  label?: string
+  labelColor?: 'white' | 'black'
+  labelFor?: string
+  value?: RadioValue
+} & InputHTMLAttributes<HTMLInputElement>
+
+const Radio = ({
+  onCheck,
+  label,
+  labelFor = '',
+  labelColor = 'white',
+  value,
+  ...props
+}: RadioProps) => {
+  const onChange = () => {
+    !!onCheck && onCheck(value)
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        id={labelFor}
+        type="radio"
+        onChange={onChange}
+        value={value}
+        {...props}
+      />
+      {!!label && (
+        <S.Label htmlFor={labelFor} labelColor={labelColor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
+
+export default Radio
---
+++ src/components/Radio/stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, StoryFn } from '@storybook/react'
+import Radio, { RadioProps } from '.'
+
+export default {
+  title: 'Radio',
+  component: Radio,
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
+  argTypes: {
+    onCheck: { action: 'checked' }
+  }
+} as Meta
+
+export const Default: StoryFn<RadioProps> = (args) => (
+  <>
+    <div style={{ padding: 10 }}>
+      <Radio
+        name="category"
+        label="Action"
+        labelFor="action"
+        isChecked
+        {...args}
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Radio name="category" label="Adventure" labelFor="adventure" {...args} />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Radio name="category" label="Strategy" labelFor="strategy" {...args} />
+    </div>
+  </>
+)
