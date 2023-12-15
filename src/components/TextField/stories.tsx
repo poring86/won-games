7eb3165619e7f6b6fac38cf71497bd6d2398be40
Matheus Lino <matheuslino86@gmail.com.br>
2023-12-15 11:08:24 -0400

    feat: :sparkles: TextField

---
+++ src/components/TextField/stories.tsx
@@ -0,0 +1,44 @@
+import { Meta, StoryFn } from '@storybook/react'
+import TextField, { TextFieldProps } from '.'
+import { AddShoppingCart } from '@styled-icons/material-outlined'
+
+export default {
+  title: 'TextField',
+  component: TextField,
+  args: {
+    label: 'E-mail',
+    labelFor: 'Email',
+    id: 'Email',
+    initialValue: '',
+    placeholder: 'john.cage@gmail.com'
+  },
+  argTypes: {
+    onInput: { action: 'changed' }
+  }
+} as Meta
+
+export const Default: StoryFn<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+export const withIcon: StoryFn<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+withIcon.args = {
+  icon: <AddShoppingCart />
+}
+
+export const withError: StoryFn<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+withError.args = {
+  error: 'Ops...something is wrong'
+}
