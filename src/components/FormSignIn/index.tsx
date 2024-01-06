726c017c19c4353bdd7b98589363ecd12cbbbae7
poring86 <matheuslino86a@gmail.com>
2024-01-06 16:50:17 -0400

    feat: :sparkles: Sign in form component

---
+++ src/components/FormSignIn/index.tsx
@@ -0,0 +1,38 @@
+import { Email, Lock } from '@styled-icons/material-outlined'
+import * as S from './styles'
+import TextField from 'components/TextField'
+import Button from 'components/Button'
+import Link from 'next/link'
+
+const FormSignIn = () => (
+  <S.Wrapper>
+    <form>
+      <TextField
+        name="email"
+        placeholder="Email"
+        type="email"
+        icon={<Email />}
+      />
+      <TextField
+        name="password"
+        placeholder="Password"
+        type="password"
+        icon={<Lock />}
+      />
+      <S.ForgotPassword href="#">Forgot your password?</S.ForgotPassword>
+
+      <Button size="large" fullWidth>
+        Sign in now
+      </Button>
+
+      <S.FormLink>
+        Don’t have an account?{' '}
+        <Link href="/sign-up">
+          <a>Sign up</a>
+        </Link>
+      </S.FormLink>
+    </form>
+  </S.Wrapper>
+)
+
+export default FormSignIn
---
+++ src/components/FormSignIn/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryFn } from '@storybook/react'
+import FormSignIn from '.'
+
+export default {
+  title: 'FormSignIn',
+  component: FormSignIn
+} as Meta
+
+export const Default: StoryFn = {}
